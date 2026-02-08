c33968f7e8858b2692fd52dc6aef35c4416b4c43
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2026-02-08 21:01:50 +0100

    feat: Paginación con RxResource

--- src/app/store-front/pages/home-page/home-page.ts
+++ src/app/store-front/pages/home-page/home-page.ts
@@ -1,9 +1,11 @@
 import { Component, inject } from '@angular/core';
-import { rxResource } from '@angular/core/rxjs-interop';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
 
 import { ProductsService } from '@products/services/products.service';
 import { ProductCard } from '@products/components/product-card/product-card';
 import { Pagination } from '@shared/components/pagination/pagination';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-home-page',
@@ -12,6 +14,19 @@ import { Pagination } from '@shared/components/pagination/pagination';
 })
 export class HomePage {
   productsService = inject(ProductsService);
+  activatedRoute = inject(ActivatedRoute);
+
+  // Tomar la ruta activa de forma dinámica y suscribirnos a los cambios que eso tenga
+  // al toSignal lo podemos inicializar con un  valor iniccial
+  currentPage = toSignal(
+    this.activatedRoute.queryParamMap.pipe(
+      map((params) => (params.get('page') ? +params.get('page')! : 1)),
+      map((page) => (isNaN(page) ? 1 : page)),
+    ),
+    {
+      initialValue: 1, // valor inicial mientras se resuelve el observable
+    },
+  );
 
   // Para hacer la petición http tan pronto ingrese, nos conectamos a un observable
   // ANTES
@@ -23,9 +38,9 @@ export class HomePage {
   // })
   // AHORA
   productsResource = rxResource({
-    params: () => ({}), // dispara la carga al crear el componente
+    params: () => ({ page: this.currentPage() - 1 }), // dispara la carga al crear el componente
     stream: ({ params }) => {
-      return this.productsService.getProducts({}); // Observable<Product[]>
+      return this.productsService.getProducts({ offset: params.page * 9 }); // Observable<Product[]>
     },
   });
   // productsResource tiene isLoading, isError, etc
