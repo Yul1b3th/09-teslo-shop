5e6ea3f25755821abb72cad5209f589eff31607e
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2026-02-08 15:50:55 +0100

    feat: Pantalla de producto

--- src/app/store-front/pages/product-page/product-page.ts
+++ src/app/store-front/pages/product-page/product-page.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '@products/services/products.service';
 
 @Component({
   selector: 'app-product-page',
   imports: [],
   templateUrl: './product-page.html',
 })
-export class ProductPage { }
+export class ProductPage {
+  // Para tomar la ruta activa
+  activateRoute = inject(ActivatedRoute);
+  productsService = inject(ProductsService);
+
+  productIdSlug: string = this.activateRoute.snapshot.params['idSLug'];
+  // productIdSlug = this.activateRoute.snapshot.paramMap.get('idSLug') || '';
+
+  productResource = rxResource({
+    params: () => ({ idSlug: this.productIdSlug }), // dispara la carga al crear el componente
+    stream: ({ params }) => {
+      return this.productsService.getProductByIdSlug(params.idSlug);
+    },
+  });
+}
