5a141468cacfcb2d12cc58e91f607309b862b475
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2026-02-08 21:17:59 +0100

    feat: Servicio de paginación

--- src/app/store-front/pages/gender-page/gender-page.ts
+++ src/app/store-front/pages/gender-page/gender-page.ts
@@ -4,21 +4,25 @@ import { ActivatedRoute } from '@angular/router';
 import { map } from 'rxjs';
 import { ProductsService } from '@products/services/products.service';
 import { ProductCard } from '@products/components/product-card/product-card';
+import { Pagination } from '@shared/components/pagination/pagination';
+import { PaginationService } from '@shared/components/pagination/pagination.servive';
 
 @Component({
   selector: 'app-gender-page',
-  imports: [ProductCard],
+  imports: [ProductCard, Pagination],
   templateUrl: './gender-page.html',
 })
 export class GenderPage {
   route = inject(ActivatedRoute);
   productsService = inject(ProductsService);
+  paginationService = inject(PaginationService);
+
   gender = toSignal(this.route.params.pipe(map(({ gender }) => gender)));
 
   productsResource = rxResource({
-    params: () => ({ gender: this.gender() }), // dispara la carga al crear el componente
+    params: () => ({ page: this.paginationService.currentPage() - 1, gender: this.gender() }), // dispara la carga al crear el componente
     stream: ({ params }) => {
-      return this.productsService.getProducts({ gender: params.gender }); // Observable<Product[]>
+      return this.productsService.getProducts({ offset: params.page * 9, gender: params.gender }); // Observable<Product[]>
     },
   });
 }
--- src/app/store-front/pages/home-page/home-page.ts
+++ src/app/store-front/pages/home-page/home-page.ts
@@ -1,11 +1,10 @@
 import { Component, inject } from '@angular/core';
-import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 import { ProductsService } from '@products/services/products.service';
 import { ProductCard } from '@products/components/product-card/product-card';
 import { Pagination } from '@shared/components/pagination/pagination';
-import { ActivatedRoute } from '@angular/router';
-import { map } from 'rxjs';
+import { PaginationService } from '@shared/components/pagination/pagination.servive';
 
 @Component({
   selector: 'app-home-page',
@@ -14,19 +13,20 @@ import { map } from 'rxjs';
 })
 export class HomePage {
   productsService = inject(ProductsService);
-  activatedRoute = inject(ActivatedRoute);
+  paginationService = inject(PaginationService);
+  // activatedRoute = inject(ActivatedRoute);
 
   // Tomar la ruta activa de forma dinámica y suscribirnos a los cambios que eso tenga
   // al toSignal lo podemos inicializar con un  valor iniccial
-  currentPage = toSignal(
-    this.activatedRoute.queryParamMap.pipe(
-      map((params) => (params.get('page') ? +params.get('page')! : 1)),
-      map((page) => (isNaN(page) ? 1 : page)),
-    ),
-    {
-      initialValue: 1, // valor inicial mientras se resuelve el observable
-    },
-  );
+  // currentPage = toSignal(
+  //   this.activatedRoute.queryParamMap.pipe(
+  //     map((params) => (params.get('page') ? +params.get('page')! : 1)),
+  //     map((page) => (isNaN(page) ? 1 : page)),
+  //   ),
+  //   {
+  //     initialValue: 1, // valor inicial mientras se resuelve el observable
+  //   },
+  // );
 
   // Para hacer la petición http tan pronto ingrese, nos conectamos a un observable
   // ANTES
@@ -38,7 +38,7 @@ export class HomePage {
   // })
   // AHORA
   productsResource = rxResource({
-    params: () => ({ page: this.currentPage() - 1 }), // dispara la carga al crear el componente
+    params: () => ({ page: this.paginationService.currentPage() - 1 }), // dispara la carga al crear el componente
     stream: ({ params }) => {
       return this.productsService.getProducts({ offset: params.page * 9 }); // Observable<Product[]>
     },
