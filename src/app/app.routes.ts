7697668b08dcbe8969ee35720beb1b8ba66cac3a
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2026-04-07 12:56:49 +0200

    feat: Rutas y páginas de autenticación

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  // loadchildren para cargar de manera perezosa la ruta auth.routes
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes'),
+    // TODO: Guards para proteger las rutas de autenticación
+  },
   {
     path: '',
     // loadchildren para cargar de manera perezosa la ruta store-front.routes
