15308c53a7eaef5838bc11582e1f4dd4636e2296
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2026-04-08 10:06:44 +0200

    feat: Formulario reactivo de autenticación

--- src/app/auth/pages/login-page/login-page.ts
+++ src/app/auth/pages/login-page/login-page.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login-page.html',
 })
-export default class LoginPage {}
+export default class LoginPage {
+  fb: FormBuilder = inject(FormBuilder);
+  hasError = signal(false);
+  isPosting = signal(false);
+
+  // Crear el formulario reactivo con FormBuilder
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  // Método para manejar el submit del formulario
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      this.hasError.set(true);
+      setTimeout(() => this.hasError.set(false), 2000); // Ocultar el error después de 3 segundos
+      return;
+    }
+
+    const { email = '', password = '' } = this.loginForm.value;
+
+    console.log({ email, password });
+  }
+}
