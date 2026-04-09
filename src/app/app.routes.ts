4a7dfffe00a278ec6d7dffaf8e686ef5f6807361
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2026-04-09 18:59:14 +0200

    feat: Guards - notAuthenticatedGuard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,18 @@
 import { Routes } from '@angular/router';
+import { NotAuthenticatedGuard } from '@auth/guard/not-authenticated.guard';
 
 export const routes: Routes = [
   // loadchildren para cargar de manera perezosa la ruta auth.routes
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.routes'),
-    // TODO: Guards para proteger las rutas de autenticación
+    canMatch: [
+      NotAuthenticatedGuard,
+      // () => {
+      //   return false; // si retorna false la ruta no se carga, debe retornar true, o null o undefined para que se cargue la ruta
+      //   // console.log('Guard global');
+      // },
+    ],
   },
   {
     path: '',
