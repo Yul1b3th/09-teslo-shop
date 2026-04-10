54445ca0757e6ea89864675bfea2d5c65a0b817e
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2026-04-10 10:32:43 +0200

    feat: Continuación

---
+++ src/app/admin-dashboard/admin-dashboard.route.ts
@@ -0,0 +1,25 @@
+import { Routes } from '@angular/router';
+import { AdminDashboardLayout } from './layouts/admin-dashboard-layout/admin-dashboard-layout';
+
+export const adminDashboardRoutes: Routes = [
+  {
+    path: '',
+    component: AdminDashboardLayout,
+    children: [
+      {
+        path: 'products',
+        loadComponent: () => import('./pages/products-admin-page/products-admin-page'),
+      },
+      {
+        path: 'products/:id',
+        loadComponent: () => import('./pages/product-admin-page/product-admin-page'),
+      },
+      {
+        path: '**',
+        redirectTo: 'products',
+      },
+    ],
+  },
+];
+
+export default adminDashboardRoutes;
