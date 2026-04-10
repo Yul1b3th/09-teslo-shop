ef91b7cd324442fc16d745795c9e932146954731
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2026-04-10 16:05:32 +0200

    feat: Mostrar productos paginados

--- src/app/admin-dashboard/pages/products-admin-page/products-admin-page.ts
+++ src/app/admin-dashboard/pages/products-admin-page/products-admin-page.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { Router } from '@angular/router';
 import { ProductTable } from '@products/components/product-table/product-table';
+import { ProductsService } from '@products/services/products.service';
+import { PaginationService } from '@shared/components/pagination/pagination.servive';
+import { Pagination } from '@shared/components/pagination/pagination';
 
 @Component({
   selector: 'app-products-admin-page',
-  imports: [ProductTable],
+  imports: [ProductTable, Pagination],
   templateUrl: './products-admin-page.html',
 })
-export default class ProductsAdminPage {}
+export default class ProductsAdminPage {
+  productsService = inject(ProductsService);
+  paginationService = inject(PaginationService);
+  router = inject(Router);
+  productsPerPage = signal(10);
+
+  productsResource = rxResource({
+    params: () => ({
+      page: this.paginationService.currentPage() - 1,
+      limit: this.productsPerPage(),
+    }), // dispara la carga al crear el componente
+    stream: ({ params }) => {
+      return this.productsService.getProducts({
+        offset: params.page * params.limit,
+        limit: params.limit,
+      }); // Observable<Product[]>
+    },
+  });
+
+  onLimitChange(newLimit: number): void {
+    this.productsPerPage.set(newLimit);
+    this.router.navigate([], { queryParams: { page: 1 } });
+  }
+}
