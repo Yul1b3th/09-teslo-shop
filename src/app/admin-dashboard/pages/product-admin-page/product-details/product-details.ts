ce3c2a01a14ce5d985dca69a0fa69239601c9800
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2026-04-11 16:33:06 +0200

    feat: Formulario de producto

--- src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.ts
+++ src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.ts
@@ -1,14 +1,44 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input, OnInit } from '@angular/core';
 import { Product } from '@products/interfaces/product.interface';
 import { ProductCarousel } from '@products/components/product-carousel/product-carousel';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '@utils/form-utils';
 
 @Component({
   selector: 'product-details',
-  imports: [ProductCarousel],
+  imports: [ProductCarousel, ReactiveFormsModule],
   templateUrl: './product-details.html',
 })
-export class ProductDetails {
+export class ProductDetails implements OnInit {
+  fb = inject(FormBuilder);
+
   product = input.required<Product>();
 
+  productForm = this.fb.group({
+    title: ['', Validators.required],
+    description: ['', Validators.required],
+    slug: ['', [Validators.required, Validators.pattern(FormUtils.slugPattern)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    stock: [0, [Validators.required, Validators.min(0)]],
+    sizes: [['']],
+    images: [[]],
+    tags: [''], // es un string, se puede guardar con comas, en la base de datos es un objeto
+    gender: ['men', [Validators.required, Validators.pattern(/men|women|kid|unisex/)]],
+  });
+
   sizes = ['XS', 'S', 'M', 'L', 'XL'];
+
+  ngOnInit(): void {
+    this.setFormValue(this.product());
+  }
+
+  setFormValue(formLike: Partial<Product>) {
+    // this.productForm.patchValue(formLike as any);
+    this.productForm.reset(this.product() as any); // Mejor el reset para que el formulario sea pristine
+    this.productForm.patchValue({ tags: formLike.tags?.join(',') });
+  }
+
+  onSubmit() {
+    console.log(this.productForm.value);
+  }
 }
